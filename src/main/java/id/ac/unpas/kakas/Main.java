8ca4c421de2cc101cc31956cb5762a51be17cc34
SB604-05 <SB604-05@DESKTOP-J0GT9QP>
2019-12-02 14:11:42 +0700

    penambahan case kurang kali bagi

--- src/main/java/id/ac/unpas/kakas/Main.java
+++ src/main/java/id/ac/unpas/kakas/Main.java
@@ -14,14 +14,26 @@ public class Main {
 		System.out.println("3. Kali");
 		System.out.println("4. Bagi");
 		menu = sc.nextInt();
-		System.out.println("Masukan n1");
+		System.out.print("Masukan n1");
 		n1 = sc.nextInt();
-		System.out.println("Masukan n1");
+		System.out.print("Masukan n1");
 		n2 = sc.nextInt();
 		Hitung hitung = new Hitung();
 		switch (menu) {
 		case 1:
 			hasil = hitung.tambah(n1, n2);
+			System.out.println(hasil);
+			break;
+		case 2:
+			hasil = hitung.kurang(n1, n2);
+			System.out.println(hasil);
+			break;
+		case 3:
+			hasil = hitung.kali(n1, n2);
+			System.out.println(hasil);
+			break;
+		case 4:
+			hasil = hitung.bagi(n1, n2);
 			System.out.println(hasil);
 			break;
 		default:
