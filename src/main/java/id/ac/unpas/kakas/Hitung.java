c57099dbd51659e39f1a108cb0c6066fc513a82f
SB604-05 <SB604-05@DESKTOP-J0GT9QP>
2019-12-02 14:03:45 +0700

    tambah & kurang

--- src/main/java/id/ac/unpas/kakas/Hitung.java
+++ src/main/java/id/ac/unpas/kakas/Hitung.java
@@ -1,5 +1,19 @@
 package id.ac.unpas.kakas;
 
 public class Hitung {
-
+	
+	public int tambah(int n1, int n2) {
+		
+        int hasil = n1+n2;
+        return hasil;
+	}
+	
+	public int kurang(int n1, int n2) {
+		
+        int hasil = n1-n2;
+        return hasil;
+	}
+	
+	
+	
 }
