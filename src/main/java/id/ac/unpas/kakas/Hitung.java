8a275696eb07649731f07ace3b4cb42fdecfe1f7
SB604-05 <SB604-05@DESKTOP-J0GT9QP>
2019-12-02 14:04:37 +0700

    kali & bagi

--- src/main/java/id/ac/unpas/kakas/Hitung.java
+++ src/main/java/id/ac/unpas/kakas/Hitung.java
@@ -14,6 +14,17 @@ public class Hitung {
         return hasil;
 	}
 	
+	public int kali(int n1, int n2) {
+		
+        int hasil = n1*n2;
+        return hasil;
+	}
+	
+	public  int bagi(int n1, int n2) {
+		
+        int hasil = n1/n2;
+        return hasil;
+	}
 	
 	
 }
