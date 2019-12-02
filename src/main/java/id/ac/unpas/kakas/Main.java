aa9e3f69615233ce49ae316544195b9d13b26388
SB604-05 <SB604-05@DESKTOP-J0GT9QP>
2019-12-02 14:05:05 +0700

    pindah kali & bagi

--- src/main/java/id/ac/unpas/kakas/Main.java
+++ src/main/java/id/ac/unpas/kakas/Main.java
@@ -2,15 +2,5 @@ package id.ac.unpas.kakas;
 
 public class Main {
 
-	public int kali(int n1, int n2) {
-		
-        int hasil = n1*n2;
-        return hasil;
-	}
-	
-	public  int bagi(int n1, int n2) {
-		
-        int hasil = n1/n2;
-        return hasil;
-	}
+
 }
